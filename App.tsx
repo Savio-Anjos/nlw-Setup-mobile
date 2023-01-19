c1455e1666f5187ea15411e9847de3673e7245b2
unknown <Savioanjos2020@gmail.com>
2023-01-18 22:41:53 -0300

    Corrigindo diretórios

--- App.tsx
+++ App.tsx
@@ -6,7 +6,7 @@ import {
   Inter_600SemiBold,
   Inter_800ExtraBold
  } from '@expo-google-fonts/inter';
-import { Loading } from './src/assets/components/Loading';
+import { Loading } from './src/components/Loading';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ 
