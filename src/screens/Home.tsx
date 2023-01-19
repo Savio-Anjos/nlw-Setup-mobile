f56acad5b2a0f5caef8f0d1d68a6b11260fbcf2c
unknown <Savioanjos2020@gmail.com>
2023-01-19 00:48:19 -0300

    Finalizando mobile aula 2

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 
 import { generateRangeDatesFromYearStart } from "../utils/generate-range-between-dates";
 
@@ -16,6 +16,8 @@ const weekDays = [
   ];
 
   const datesFromYearStart = generateRangeDatesFromYearStart();
+  const minimumSummaryDateSizes = 18 * 5;
+  const amountOfDaysToFill = minimumSummaryDateSizes - datesFromYearStart.length
 
 export function Home() {
     return (
@@ -34,6 +36,10 @@ export function Home() {
                     )))
                 }
             </View>
+            <ScrollView
+            showsVerticalScrollIndicator={false}
+            contentContainerStyle={{paddingBottom: 100}}
+            >
              <View className="flex-row flex-wrap">
             {
                 datesFromYearStart.map(date => (
@@ -42,7 +48,20 @@ export function Home() {
                     />
                 ))
             }
+
+{
+                amountOfDaysToFill > 0 && Array
+                .from({ length: amountOfDaysToFill })
+                .map((_, index) => (
+                    <View 
+                    className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
+                    style={{width: DAY_SIZE, height: DAY_SIZE }}
+                    />
+                ) )
+            }
             </View>
+            </ScrollView>
+           
 
         </View>
     )
