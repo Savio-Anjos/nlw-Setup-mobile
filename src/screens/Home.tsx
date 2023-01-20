75d6be528f77d98232fe1f87996eecf73eb70b40
unknown <Savioanjos2020@gmail.com>
2023-01-20 10:38:57 -0300

    Configurando Habit

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,4 +1,5 @@
 import { View, Text, ScrollView } from "react-native";
+import { useNavigation } from '@react-navigation/native'
 
 import { generateRangeDatesFromYearStart } from "../utils/generate-range-between-dates";
 
@@ -20,6 +21,9 @@ const weekDays = [
   const amountOfDaysToFill = minimumSummaryDateSizes - datesFromYearStart.length
 
 export function Home() {
+
+    const { navigate } = useNavigation();
+
     return (
         <View className="flex-1 bg-background px-8 pt-16">
             <Header />
@@ -45,6 +49,7 @@ export function Home() {
                 datesFromYearStart.map(date => (
                     <HabitDay
                     key={date.toISOString()} 
+                    onPress={() => navigate('habit', {date: date.toISOString() })}
                     />
                 ))
             }
