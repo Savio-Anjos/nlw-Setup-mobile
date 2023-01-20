d57318409e92b07a750e8c945c5df89bca116983
unknown <Savioanjos2020@gmail.com>
2023-01-20 09:10:51 -0300

    Criando botão para confirmar

--- src/screens/New.tsx
+++ src/screens/New.tsx
@@ -24,7 +24,7 @@ export function New() {
         <View className="flex-1 bg-background px-8 pt-16">
             <ScrollView 
             showsVerticalScrollIndicator={false}
-            contentContainerStyle={{ paddingBottom: 100 }}
+            contentContainerStyle={{ paddingBottom: 100 }} 
             >
 
                 <BackButton />
