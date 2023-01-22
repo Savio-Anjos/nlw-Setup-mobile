df63fb460c497775209fc0612e44826a4da01cc8
unknown <Savioanjos2020@gmail.com>
2023-01-22 11:28:25 -0300

    Criando função para calcular a porcentagem do progresso

--- src/screens/New.tsx
+++ src/screens/New.tsx
@@ -1,82 +1,99 @@
 import { useState } from "react";
-import { ScrollView, Text, TextInput, TouchableOpacity, View } from "react-native";
-import { Feather } from '@expo/vector-icons';
+import { Alert, ScrollView, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { Feather } from '@expo/vector-icons'
+import colors from "tailwindcss/colors";
 
 import { BackButton } from "../components/BackButton";
 import { Checkbox } from "../components/Checkbox";
-import colors from "tailwindcss/colors";
+import { api } from "../lib/axios";
 
-const avaiableWeekDays = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado']
+const availableWeekDays = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado'];
 
 export function New() {
-    const [weekDays, setWeekDays] = useState<number[]>([]);
+  const [weekDays, setWeekDays] = useState<number[]>([]);
+  const [title, setTitle] = useState('');
 
-    function handleToggleWeekDay(weekDayIndex: number) {
-        if(weekDays.includes(weekDayIndex)) {
-            setWeekDays(prevState => prevState.filter(weekDay => weekDay !== weekDayIndex));
-
-        } else {
-            setWeekDays(prevState => [...prevState, weekDayIndex]);
-        }
+  function handleToggleWeekDay(weekDayIndex: number) {
+    if (weekDays.includes(weekDayIndex)) {
+      setWeekDays(prevState => prevState.filter(weekDay => weekDay !== weekDayIndex))
+    } else {
+      setWeekDays(prevState => [...prevState, weekDayIndex])
     }
+  }
 
-    return (
-        <View className="flex-1 bg-background px-8 pt-16">
-            <ScrollView 
-            showsVerticalScrollIndicator={false}
-            contentContainerStyle={{ paddingBottom: 100 }} 
-            >
+  async function handleCreateNewHabit() {
+    try {
+      if (!title.trim() || weekDays.length === 0) {
+        Alert.alert('Novo hábito', 'Informe o nome do hábito e escolha a periodicidade.')
+      }
 
-                <BackButton />
+      await api.post('/habits', { title, weekDays })
+      
+      setTitle('');
+      setWeekDays([]);
 
-                <Text className="mt-6 text-white font-extrabold text-3xl">
-                    Criar hábito
-                </Text>
+      Alert.alert('Novo hábito', 'Hábito criado com sucesso!');
+    } catch (error) {
+      console.log(error)
+      Alert.alert('Ops', 'Não foi possível criar o novo hábito')
+    }
+  }
 
-                <Text className="mt-6 text-white font-semibold text-base">
-                    Qual o seu comprometimento?
-                </Text>
+  return (
+    <View className="flex-1 bg-background px-8 pt-16">
+      <ScrollView 
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
+      >
+        <BackButton />
 
-                <TextInput 
-                className="h-12 pl-4 rounded-lg mt-3 bg-zinc-900 text-white border-2 border-zinc-800 focus:border-green-600"
-                placeholder="Exercícios, dormir bem, etc..."
-                placeholderTextColor={colors.zinc[400]}
-                />
+        <Text className="mt-6 text-white font-extrabold text-3xl">
+          Criar hábito
+        </Text>
 
-                <Text className="font-semibold mt-4 mb-3 text-white text-base">
-                    Qual a recorrência?
-                </Text>
+        <Text className="mt-6 text-white font-semibold text-base">
+          Qual seu comprometimento?
+        </Text>
 
-                 {
-                    avaiableWeekDays.map((weekDay, index) => (
-                        <Checkbox 
-                        key={weekDay}
-                        title={weekDay}
-                        checked={weekDays.includes(index)}
-                        onPress={() => handleToggleWeekDay(index)}
+        <TextInput 
+          className="h-12 pl-4 rounded-lg mt-3 bg-zinc-900 text-white border-2 border-zinc-800 focus:border-green-600"
+          placeholder="Exercícios, dormir bem, etc..."
+          placeholderTextColor={colors.zinc[400]}
+          onChangeText={setTitle}
+          value={title}
+        />
 
-                    />
-                    ))
-                 }
+        <Text className="font-semibold mt-4 mb-3 text-white text-base">
+          Qual a recorrência?
+        </Text>
 
-                 <TouchableOpacity
-                 className="2-full h-14 flex-row items-center justify-center bg-green-600 rounded-md mt-6"
-                 activeOpacity={0.7}
-                 >
-                    <Feather 
-                       name="check"
-                       size={20}
-                       color={colors.white}
-                    />
+        {
+          availableWeekDays.map((weekDay, index) => (
+            <Checkbox
+              key={weekDay}
+              title={weekDay}
+              checked={weekDays.includes(index)}
+              onPress={() => handleToggleWeekDay(index)}
+            />
+          ))
+        }
 
-                    <Text className="font-semibold text-base text-white ml-2">
-                        Confirmar
-                    </Text>
-                 </TouchableOpacity>
-               
-                
-            </ScrollView>
+        <TouchableOpacity
+          className="w-full h-14 flex-row items-center justify-center bg-green-600 rounded-md mt-6"
+          activeOpacity={0.7}
+          onPress={handleCreateNewHabit}
+        >
+          <Feather 
+           name="check"
+           size={20}
+           color={colors.white}
+          />
 
-        </View>
-    )
+          <Text className="font-semibold text-base text-white ml-2">
+            Confirmar
+          </Text>
+        </TouchableOpacity>
+      </ScrollView>
+    </View>
+  )
 }
--- src/utils/generate-progress-porcentage.ts
+++
@@ -1,3 +0,0 @@
-export function genereteProgressPorcentage(total: number, completed: number) {
-    return Math.round((completed / total) * 100);
-}
